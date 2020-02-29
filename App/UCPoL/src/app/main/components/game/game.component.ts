7ee6acd3c79e7fddf8cfa5d6ce21363223de813b
Hkozacz <hkozacz@gmail.com>
2020-02-29 13:27:34 +0100

    simple hp bar

--- App/UCPoL/src/app/main/components/game/game.component.ts
+++ App/UCPoL/src/app/main/components/game/game.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { BarsComponent } from '../bars/bars.component';
+
+
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
+
+
 export class GameComponent implements OnInit {
 
-  constructor() { }
-
+  bars = new BarsComponent(); 
+  constructor() {
+    
+   }
+  
   ngOnInit(): void {
+    
+  }
+  changeHP(amount: number):void{
+    this.bars.changeHP(amount);
   }
 
 }
