26d0d730aba340555353fe19f907b6da1bd8743a
Hkozacz <hkozacz@gmail.com>
2020-02-29 13:07:02 +0100

    simple hp bar

--- App/UCPoL/src/app/main/components/bars/bars.component.ts
+++ App/UCPoL/src/app/main/components/bars/bars.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarsComponent implements OnInit {
 
-  constructor() { }
+  hp = 100;
+  constructor() {
+
+   }
 
   ngOnInit(): void {
+    document.documentElement.style.setProperty('--hpPrct', `${this.hp}%`);
+    document.documentElement.style.setProperty('--hpPrctd', `${this.hp-100}%`);
+    console.log(document.documentElement.style.getPropertyValue('--hpPrct'))
+  }
+
+  changeHP(amount):void{
+    if(this.hp > 0 && amount < 0 || this.hp < 100 && amount > 0){
+    this.hp += amount;
+    document.documentElement.style.setProperty('--hpPrct', `${this.hp}%`);
+    }
+
+    console.log(this.hp);
   }
 
 }
